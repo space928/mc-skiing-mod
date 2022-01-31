5a9e4e18c7a4ae2731b879ec57d84fcb5be6975e
Adam Mathieson <adam@genav.ch>
2022-01-31 13:08:50 +0000

    + Wrench click handler

--- src/main/java/com/mcskiingmod/items/ItemSwissArmyKnife.java
+++ src/main/java/com/mcskiingmod/items/ItemSwissArmyKnife.java
@@ -1,33 +1,65 @@
 package com.mcskiingmod.items;
 
+import com.mcskiingmod.SkiingModTab;
 import net.minecraft.block.Block;
 import net.minecraft.client.resources.Locale;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 
+import static com.mcskiingmod.Main.SKIING_MOD_TAB;
+
 public class ItemSwissArmyKnife extends ItemBase {
+    private BlockPos SelectedBlockPos;
+    private Block SelectedBlock;
+    private Timer clickTimer;
 
     public ItemSwissArmyKnife(String name) {
         super(name);
     }
 
     @Override
-    public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float x, float y, float z) {
+    public void onUpdate(ItemStack itemStack, World world, Entity entity, int itemSlot, boolean isSelected) {
+        if (isSelected&&clickTimer != null)
+            clickTimer.updateTimer();
+        super.onUpdate(itemStack, world, entity, itemSlot, isSelected);
+    }
+
+    @Override
+    public EnumActionResult onItemUseFirst(EntityPlayer player, World world, BlockPos pos, EnumFacing facing, float x, float y, float z, EnumHand hand) {
+        if (clickTimer == null) {
+            clickTimer = new Timer(20);
+        } else {
+            if (clickTimer.elapsedTicks < 1)
+                return super.onItemUseFirst(player, world, pos, facing, x, y, z, hand);
+        }
+
         TileEntity te = world.getTileEntity(pos);
         if (te != null) {
             Block block = te.getBlockType();
-            player.sendMessage(new TextComponentString("Selected '" + block.getLocalizedName() + "' @ " + pos));
-            return EnumActionResult.PASS;
+            if (block.getCreativeTab() == SKIING_MOD_TAB) {
+                player.sendStatusMessage(new TextComponentString("Selected '" + block.getLocalizedName() + "' @ " + pos), true);
+                if (SelectedBlock == null) {
+                    SelectedBlock = block;
+                    SelectedBlockPos = pos;
+                } else {
+                    // Run Wand stuff - Currently distance measurement test code, to be replaced with linking code
+                    player.sendStatusMessage(new TextComponentString("Distance Between Points: " + new Vec3d(SelectedBlockPos).distanceTo(new Vec3d(pos)) + "m"), true);
+
+
+                    SelectedBlock = null;
+                    SelectedBlockPos = null;
+                }
+                return EnumActionResult.PASS;
+            }
         }
-        return super.onItemUse(player, world, pos, hand, facing, x, y, z);
+        return super.onItemUseFirst(player, world, pos, facing, x, y, z, hand);
     }
 
     @Override
